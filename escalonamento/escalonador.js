2c074b9dcd7537f3e8954fea34516b12a3d9ffd1
edivaldoleitao <52206423+edivaldoleitao@users.noreply.github.com>
2023-02-03 19:34:07 -0300

    commit 2

---
+++ escalonamento/escalonador.js
@@ -0,0 +1,49 @@
+(function (window, document) { //IIFE immediately invoked function expression
+    
+    let Simulador
+    let StatusSimulador = ['Parado', 'Executando','Finalizado']
+
+    let statusProcesso = {
+        NOVO: 0,
+        PROCESSANDO: 1,
+        AGUARDANDO: 2,
+        CONCLUIDO:3,
+        ESPERANDO_IO:4,
+    }
+
+    let btnExecutarSimulador = document.querySelector('#btnExecutar')
+    let statusSimulador = document.querySelector('#simulatorStatus')
+
+    btnExecutarSimulador.addEventListener("click",function(){Simulador.iniciar()})
+
+    Simulador = {
+        Status: null,
+
+        quantum: document.querySelector('#quantum'),
+        processPerMin: document.querySelector('#ppm'),
+        processTime: document.querySelector('#lifeTime'),
+        ioChance: document.querySelector('#ioChance'),
+        waitTimeIO: document.querySelector('#waitTimeIO'),
+        simuladorStatus: document.querySelector('#simuladorStatus'),
+        cycles: document.querySelector('#cycles'),
+        createdProcess: document.querySelector('#createdProcess'),
+        completedProcess: document.querySelector('#completedProcess'),
+        IoProcessQtd: document.querySelector('#IoProcess'),
+        processTable: document.querySelector('#processTable'),
+
+        iniciar: function () {
+            this.processTable.innerHTML = ''
+
+            if (this.validaParametros()) {
+                //code
+            }
+            
+        },
+
+        validaParametros: function (params) {
+            return validaInput(1, this.quantum, this.processPerMin, this.waitTimeIO, this.processTime) &&
+            validaInput(0, this.ioChance)
+        },
+    }
+}(window, document))
+
